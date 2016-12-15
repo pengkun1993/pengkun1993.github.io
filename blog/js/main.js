d1537822c68c7bd25a2c2a7b4c6cbd70abe584f2
yaopengkun <782041896@qq.com>
2016-12-15 10:54:53 +0800

    1215

--- blog/js/main.js
+++ blog/js/main.js
@@ -7,4 +7,10 @@ $(function(){
 		navigationColor:"#fff",
 		scrollOverflow:true,
 	});
+	//li颜色渐深
+	$('#three li').each(function(index,event){
+		index=index%5;
+		var rgb=150-index*30;
+		$(event).css({'background-color':'rgba('+rgb+','+rgb+','+rgb+',0.3)'});
+	})
 });
