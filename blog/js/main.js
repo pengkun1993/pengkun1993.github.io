1b71de5e93a11900d99ed611eceb83b8f6f7b552
yaopengkun <782041896@qq.com>
2016-12-16 16:02:55 +0800

    bootstrop

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,4 +1,14 @@
 $(function(){
+	//li颜色渐深
+	$('#three li').each(function(index,event){
+		index=index%5;
+		var rgb=150-index*30;
+		$(event).css({'background-color':'rgba('+rgb+','+rgb+','+rgb+',0.3)'});
+	})
+	//canvas方块
+	var c_w=$('#two .icon').css('width');
+	$('#two .icon').css({'height':c_w,'line-height':c_w});
+	
 	//fullpage插件设置
 	$('#fullpage').fullpage({
 		resize:true,
@@ -7,10 +17,4 @@ $(function(){
 		navigationColor:"#fff",
 		scrollOverflow:true,
 	});
-	//li颜色渐深
-	$('#three li').each(function(index,event){
-		index=index%5;
-		var rgb=150-index*30;
-		$(event).css({'background-color':'rgba('+rgb+','+rgb+','+rgb+',0.3)'});
-	})
 });
