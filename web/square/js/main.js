6e5e425e8c429b3044ce855afbd55ec9d94b3c0b
yaopengkun <782041896@qq.com>
2016-12-20 11:32:24 +0800

    chukog

---
+++ web/square/js/main.js
@@ -0,0 +1,46 @@
+var state;
+var start_x;
+var start_y;
+//转换像素到角度
+var range_x;
+var range_y;
+
+var x_angel;
+var y_angle;
+$(function(){
+	state=0;
+	x_angel=0;
+	y_angle=0;
+	$(document).mousedown(function(e){
+		e.preventDefault();
+		if(state==0){
+			state=1;
+			start_x=e.pageX;
+			start_y=e.pageY;
+		}
+	});
+	$(document).mouseup(function(e){
+		e.preventDefault();
+		state=0;
+	});
+	$(document).mousemove(function(e){
+		e.preventDefault();
+		if(state==1){
+			rotate_square(e.pageX,e.pageY);	
+		}
+	});
+	$(document).mouseout(function(e){
+		e.preventDefault();
+		state=0;
+	});
+});
+function rotate_square(x,y){
+	var del_x=(x-start_x)%10;
+	var del_y=(y-start_y)%10;
+	
+	x_angel+=del_x;
+	y_angle+=del_y;
+	$('#square').css({'transform':'rotateY('+ x_angel +'deg) rotateX('+ -y_angle +'deg) '});
+	start_x=x;
+	start_y=y;
+}
